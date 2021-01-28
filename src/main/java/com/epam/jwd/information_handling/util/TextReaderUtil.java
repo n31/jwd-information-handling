d65ce2c3b6f8e53972964d851887934370a2b587
n31 <nikkovlev@gmail.com>
2021-01-28 19:52:56 +0300

    Add file reader

---
+++ src/main/java/com/epam/jwd/information_handling/util/TextReaderUtil.java
@@ -0,0 +1,12 @@
+package com.epam.jwd.information_handling.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TextReaderUtil {
+    private static final Logger logger = LogManager.getLogger(TextReaderUtil.class);
+
+    static String read(String filePath) {
+        return null;
+    }
+}
