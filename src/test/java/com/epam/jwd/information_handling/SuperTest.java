d14f3cdf5abc69514a0468ce28dedd2042c5f325
n31 <nikkovlev@gmail.com>
2021-01-30 23:21:32 +0300

    Add main test

--- src/test/java/com/epam/jwd/information_handling/SuperTest.java
+++ src/test/java/com/epam/jwd/information_handling/SuperTest.java
@@ -2,12 +2,14 @@ package com.epam.jwd.information_handling;
 
 import com.epam.jwd.information_handling.domain.BaseTextComponent;
 import com.epam.jwd.information_handling.domain.EntireText;
+import com.epam.jwd.information_handling.util.TextReaderUtil;
 import org.junit.Test;
 
 public class SuperTest {
     @Test
     public void test() {
         String str = "Hello nick. How's are u?\nFine.";
+        str = TextReaderUtil.read("src/main/resources/text.txt");
         BaseTextComponent root = new EntireText();
         root.setValue(str);
         root.add(root);
